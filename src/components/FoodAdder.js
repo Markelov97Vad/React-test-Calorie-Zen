78266119e73f0454acd3bb90e3bda2749bfa2395
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-03-01 15:50:53 +0300

    feat: функция добавления колорий

--- src/components/FoodAdder.js
+++ src/components/FoodAdder.js
@@ -5,18 +5,31 @@ class FoodAdder extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      foodList: []
+      food: '',
+      calories: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit = () => {
+  handleChange = (el) => {
+    this.setState({
+      [el.target.name]: el.target.value,
+    })
+  }
 
+  handleSubmit = (el) => {
+    el.preventDefault();
+
+    this.setState({food: '', colories: ''});
+    this.props.handleSubmit(this.state.food, this.state.calories);
   }
 
   render() {
     return (
-      <form className="food-adder">
-        <input type="text" placeholder="Добавьте продукты" />
+      <form className="food-adder" onSubmit={this.handleSubmit}>
+        <input name='food' onChange={this.handleChange} value={this.state.food} type="text" placeholder="Добавьте продукты" />
+        <input name="calories" onChange={this.handleChange} value={this.state.calories} type="number" required placeholder="Калории" />
         <button className="food-adder__submit"></button>
       </form>
     );
